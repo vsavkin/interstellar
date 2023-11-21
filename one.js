024e8b70553802ee78e136d59242844b13b6c291
Victor Savkin <mail@vsavkin.com>
2023-11-21 10:59:10 -0500

    wip

--- one.js
+++ one.js
@@ -5,8 +5,8 @@ const projects = JSON.parse(fs.readFileSync("_workspace.json")).projects
 
 for (let p of Object.keys(projects)) {
   const projectJson = JSON.parse(fs.readFileSync(projects[p] + '/project.json'))
-  projectJson.targets.test = {}
-  projectJson.targets.lint = {}
-  projectJson.targets.e2e = {}
+  if (projectJson.e2e && Object.keys(projectJson.e2e).length == 0) {
+    delete projectJson.targets.e2e
+  }
   fs.writeFileSync(projects[p] + '/project.json', JSON.stringify(projectJson, null, 2))
 }
