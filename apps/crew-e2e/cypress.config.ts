f5567293904ccfcdefdc4a858cf3ddc6ce3bcff1
Victor Savkin <mail@vsavkin.com>
2023-11-21 14:22:50 -0500

    wip

--- apps/crew-e2e/cypress.config.ts
+++ apps/crew-e2e/cypress.config.ts
@@ -1,6 +1,10 @@
-import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
-import { defineConfig } from 'cypress';
+import {nxE2EPreset} from '@nx/cypress/plugins/cypress-preset';
+import {defineConfig} from 'cypress';
 
 export default defineConfig({
-  e2e: nxE2EPreset(__filename, { cypressDir: 'src' }),
+  e2e: nxE2EPreset(__filename, {
+    cypressDir: 'src',
+    devServerTargets: {default: 'crew:serve'},
+    ciDevServerTarget: 'crew:serve-static'
+  }),
 });
